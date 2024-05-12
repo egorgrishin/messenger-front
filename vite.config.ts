795af176c680c065ae8027e5b939ddcd9ddc5e21
Egor Grishin <egor-grishin@bk.ru>
2024-05-13 00:38:38 +0300

    aliases

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "api": '/src/api',
+      "assets": '/src/assets',
+      "components": '/src/components',
+      "composables": '/src/composables',
+      "interfaces": '/src/interfaces',
+      "layouts": '/src/layouts',
+      "routes": '/src/routes',
+      "services": '/src/services',
+      "types": '/src/types',
+      "views": '/src/views',
+    },
+  },
 })
